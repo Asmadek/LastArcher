18aabccf72825450961ca787e11407bc9e655ccf
Day7 <day345987@gmail.com>
2016-11-04 00:10:22 +0300

    Camera out after 1 seconds, started to do bars

---
+++ LastArcher/Bar.swift
@@ -0,0 +1,14 @@
+//
+//  Bar.swift
+//  LastArcher
+//
+//  Created by Day7 on 03.11.16.
+//  Copyright © 2016 AAAR. All rights reserved.
+//
+
+import SpriteKit
+
+class Bar: SKSpriteNode {
+    var BarWidth: CGFloat = 40
+    var BarHeight: CGFloat = 4
+}
--- LastArcher/Joystick.swift
+++ LastArcher/Joystick.swift
@@ -11,20 +11,23 @@ import SpriteKit
 class Joystick{
     let moveAnalogStick =  🕹(diameter: 150)
     let shootAnalogStick = AnalogJoystick(diameter: 150)
-    let cameraZoomInAction = SKAction.scale(to: 1.5, duration: 0.5)
-    let cameraZoomOutAction = SKAction.scale(to: 1, duration: 0.5)
+    let cameraZoomOutAction = SKAction.scale(to: 1.5, duration: 0.5)
+    let cameraZoomInAction = SKAction.scale(to: 1, duration: 0.5)
+ //   let chargeBowStringAction = SKAction.moveBy(x: <#T##CGFloat#>, y: <#T##CGFloat#>, duration: <#T##TimeInterval#>)
     
     var chargeTime = TimeInterval(0.0)
     var mainScene:GameScene
     var archer:Archer
     var camera:SKCameraNode
     var pullForce:CGFloat
+    var cameraTriggered:Bool
     
     init(){
         self.mainScene = GameScene.mainScene!
         self.archer = mainScene.archer
         self.camera = mainScene.camera!
         self.pullForce = 0.0
+        self.cameraTriggered = false
         
         moveAnalogStick.position = CGPoint(
             x: mainScene.frame.minX * 0.65,
@@ -48,12 +51,15 @@ class Joystick{
     
     func shootStartHandler() {
         archer.pullBowstring()
-        camera.run(cameraZoomInAction)
-    }
+        print(archer.chargeTime)
+
+            }
     
     func shootStopHandler() {
         archer.releaseBowstring(pullForce: self.pullForce)
-        camera.run(cameraZoomOutAction)
+        camera.run(cameraZoomInAction)
+        print(archer.chargeTime)
+        cameraTriggered = false
     }
     
     func moveTrackingHandler(data: AnalogJoystickData) {
@@ -68,6 +74,10 @@ class Joystick{
         if (pullForce > archer.MIN_PULL_FORCE){
             archer.turn(direction: data.velocity.multiply(scalar: -1))
 
+        }
+        if (((NSDate.timeIntervalSinceReferenceDate - archer.chargeTime) > 0.7) && (cameraTriggered == false)){
+            camera.run(cameraZoomOutAction)
+            cameraTriggered = true
         }
     }
 }
